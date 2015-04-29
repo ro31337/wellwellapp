37758ce0c8a5f2c0f985c0724701d458e43f22c1
Roman Pushkin <roman.pushkin@gmail.com>
2015-04-28 17:59:16 -0700

    Cleanup server startup and create index for Cards boardId property

--- server/startup.js
+++ server/startup.js
@@ -1,14 +1,3 @@
 Meteor.startup(function () {
-  if (!Boards.findOne()) {
-    
-    var boards = [
-      {title: 'Board 1', body: 'This is board 1'},
-      {title: 'Board 2', body: 'This is board 2'},
-      {title: 'Board 3', body: 'This is board 3'}
-    ];
-
-    boards.forEach(function (board) {
-      Boards.insert(board);
-    });
-  }
+  Cards._ensureIndex({ 'boardId': 1 }, { background: true });
 });
